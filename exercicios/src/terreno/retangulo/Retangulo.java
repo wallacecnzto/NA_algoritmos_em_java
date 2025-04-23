4d4f7e2202735a79b48bd5eb02b78e6f7131b297
Wallace Mariano <wallacewvinfo@gmail.com>
2025-04-23 17:54:24 -0300

    Exercise retangulo solved.

---
+++ exercicios/src/terreno/retangulo/Retangulo.java
@@ -0,0 +1,36 @@
+package terreno.retangulo;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Retangulo {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+        
+        double base;
+        double altura;
+        double area;
+        double perimetro;
+        double diagonal;
+
+        System.out.println("Base do retangulo:");
+        base = scanner.nextDouble();
+
+        System.out.println("Altura do retangulo: ");
+        altura = scanner.nextDouble();
+        
+        area = base * altura;
+        perimetro = 2 * (base + altura);
+        diagonal = Math.sqrt((base * base) + (altura * altura));
+
+        System.out.printf("AREA = %.4f\n", area);
+        System.out.printf("PERIMETRO = %.4f\n", perimetro);
+        System.out.printf("DIAGONAL = %.4f\n", diagonal);
+        
+        scanner.close();
+
+    }
+}
